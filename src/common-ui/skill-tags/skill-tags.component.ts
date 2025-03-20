a02ed9beb62f2e11357267e765e027ad6c4efbb1
az4ert85 <vitaliy.chustyak@gmail.com>
2025-03-20 08:54:42 +0200

    Add skill-tags component

---
+++ src/common-ui/skill-tags/skill-tags.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-tags',
+  imports: [],
+  templateUrl: './skill-tags.component.html',
+  styleUrl: './skill-tags.component.scss'
+})
+export class SkillTagsComponent {
+
+}
